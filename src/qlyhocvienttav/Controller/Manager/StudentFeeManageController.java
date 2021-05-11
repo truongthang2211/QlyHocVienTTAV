a798b39a0861cbf247deec4d3f96a44a4c3a1d7f
sanke2211@users <59715444+sanke2211@users.noreply.github.com>
2021-05-11 12:43:16 +0700

    fix studentfeecontroller

--- src/qlyhocvienttav/Controller/Manager/StudentFeeManageController.java
+++ src/qlyhocvienttav/Controller/Manager/StudentFeeManageController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,88 +5,20 @@
  */
 package qlyhocvienttav.Controller.Manager;
 
-import com.jfoenix.controls.JFXComboBox;
-import com.jfoenix.controls.JFXDatePicker;
-import com.jfoenix.controls.JFXTextField;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-=======
-package qlyhocvienttav.Controller.Manager;
 
-import javafx.collections.ObservableArray;
->>>>>>> db342fce126da7233efcdf26134bdee96c43c706
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-<<<<<<< HEAD
-import javafx.scene.control.TableView;
-import qlyhocvienttav.Model.DAL.TestSche_DAL;
 
 /**
  * FXML Controller class
  *
  * @author Thang
  */
-public class StudentFeeManageController implements Initializable {
-    public ObservableList<?> data;
-    @FXML
-    private Label topcenterlabel;
-    @FXML
-    private TableView<?> maintable;
-    @FXML
-    private JFXTextField Searchtxt;
-    @FXML
-    private JFXTextField FeeIdtxt;
-    @FXML
-    private JFXTextField StudentIDtxt;
-    @FXML
-    private JFXTextField FullNametxt;
-    @FXML
-    private JFXTextField FeeCompletedtxt;
-    @FXML
-    private JFXDatePicker Timetxt;
-    @FXML
-    private JFXComboBox<String> StatusCbb;
-    
-    TestSche_DAL testsche_dal = new TestSche_DAL();
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        ObservableList<String> StatusList = FXCollections.observableArrayList("Hoàn thành","Chưa hoàn thành");
-        StatusCbb.setItems(StatusList);
-        
-       
-    }    
 
-    @FXML
-    private void AddBtn(ActionEvent event) {
-        
-    }
-
-    @FXML
-    private void EditBtn(ActionEvent event) {
-    }
-
-    @FXML
-    private void DeleteBtn(ActionEvent event) {
-    }
-    
-=======
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import qlyhocvienttav.Model.DAL.StudentFee_DAL;
-import qlyhocvienttav.Model.DAL.Teacher_DAL;
-import qlyhocvienttav.Model.DTO.Student;
 import qlyhocvienttav.Model.DTO.StudentFee;
-import qlyhocvienttav.Model.DTO.Teacher;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
@@ -95,6 +26,11 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class StudentFeeManageController implements Initializable {
     public ObservableList<StudentFee> data;
@@ -194,5 +130,4 @@ public class StudentFeeManageController implements Initializable {
 
 
     }
->>>>>>> db342fce126da7233efcdf26134bdee96c43c706
 }
